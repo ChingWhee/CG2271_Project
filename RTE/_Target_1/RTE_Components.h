d7f11984c02099fce42ea0e64cec04e315745b48
roshnidaksha <groshnidaksha2@yahoo.com>
2025-03-21 16:53:59 +0800

    Motion Control with PWM

--- RTE/_Target_1/RTE_Components.h
+++ RTE/_Target_1/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'CG2271' 
+ * Project: 'ProjectMotorControl' 
  * Target:  'Target 1' 
  */
 
---
+++ motor.c
@@ -0,0 +1,203 @@
+#include "motor.h"
+
+void initMotor(void) {
+	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;
+	SIM_SCGC5 |= SIM_SCGC5_PORTE_MASK;
+	
+	// Enable Clock Gating for TPM0 and TPM1
+	SIM->SCGC6 |= SIM_SCGC6_TPM0_MASK;
+  SIM->SCGC6 |= SIM_SCGC6_TPM1_MASK;
+	
+	// Configure correct modes for PWM Pin Operation
+	PORTD->PCR[RIGHT_FRONT_PWM_FWD] &= ~PORT_PCR_MUX_MASK;
+  PORTD->PCR[RIGHT_FRONT_PWM_FWD] |= PORT_PCR_MUX(4);
+
+	PORTD->PCR[RIGHT_FRONT_PWM_BWD] &= ~PORT_PCR_MUX_MASK;
+  PORTD->PCR[RIGHT_FRONT_PWM_BWD] |= PORT_PCR_MUX(4);
+	
+	PORTD->PCR[RIGHT_BACK_PWM_FWD] &= ~PORT_PCR_MUX_MASK;
+  PORTD->PCR[RIGHT_BACK_PWM_FWD] |= PORT_PCR_MUX(4);
+	
+	PORTD->PCR[RIGHT_BACK_PWM_BWD] &= ~PORT_PCR_MUX_MASK;
+  PORTD->PCR[RIGHT_BACK_PWM_BWD] |= PORT_PCR_MUX(4);
+	
+	PORTE->PCR[LEFT_FRONT_PWM_FWD] &= ~PORT_PCR_MUX_MASK;
+  PORTE->PCR[LEFT_FRONT_PWM_FWD] |= PORT_PCR_MUX(3);
+	
+	PORTE->PCR[LEFT_FRONT_PWM_BWD] &= ~PORT_PCR_MUX_MASK;
+  PORTE->PCR[LEFT_FRONT_PWM_BWD] |= PORT_PCR_MUX(3);
+	
+	PORTE->PCR[LEFT_BACK_PWM_FWD] &= ~PORT_PCR_MUX_MASK;
+  PORTE->PCR[LEFT_BACK_PWM_FWD] |= PORT_PCR_MUX(3);
+	
+	PORTE->PCR[LEFT_BACK_PWM_BWD] &= ~PORT_PCR_MUX_MASK;
+  PORTE->PCR[LEFT_BACK_PWM_BWD] |= PORT_PCR_MUX(3);
+	
+	// Set Modulo Value 48000000 / 128 = 375000 / 50Hz = 7500
+  TPM1->MOD = PWM_PERIOD;
+	
+	/* Edge-Aligned PWM */
+  // Update Sns register: CMOD = 01, PS = 111 (128)
+	TPM1->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
+	TPM1->SC |= (TPM_SC_CMOD(1) | TPM_SC_PS(7));
+	TPM1->SC &= ~(TPM_SC_CPWMS_MASK);
+	
+	TPM0->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
+	TPM0->SC |= (TPM_SC_CMOD(1) | TPM_SC_PS(7));
+	TPM0->SC &= ~(TPM_SC_CPWMS_MASK);
+	
+	//Enable PWM on TPM0 Channel 0 -> PTD0
+	TPM0_C0SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM0_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	
+	//Enable PWM on TPM0 Channel 1 -> PTD1
+	TPM0_C1SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM0_C1SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	
+	//Enable PWM on TPM0 Channel 4 -> PTD4
+	TPM0_C4SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM0_C4SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	
+	//Enable PWM on TPM0 Channel 5 -> PTD5
+	TPM0_C5SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM0_C5SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	
+	//Enable PWM on TPM1 Channel 0 -> PTE20
+	TPM1_C0SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM1_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	
+	//Enable PWM on TPM1 Channel 1 -> PTE21
+	TPM1_C1SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM1_C1SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	
+	//Enable PWM on TPM0 Channel 2 -> PTE29
+	TPM0_C2SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM0_C2SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	
+	//Enable PWM on TPM0 Channel 3 -> PTE30
+	TPM0_C3SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM0_C3SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+}
+
+void move_right_forward_wheel(uint16_t speed, uint8_t direction) {
+	if (direction) {
+		TPM0_C0V = speed;
+		TPM0_C1V = 0;
+	} else {
+		TPM0_C0V = 0;
+		TPM0_C1V = speed;
+	}
+}
+
+void move_right_backward_wheel(uint16_t speed, uint8_t direction) {
+	if (direction) {
+		TPM0_C4V = speed;
+		TPM0_C5V = 0;
+	} else {
+		TPM0_C4V = 0;
+		TPM0_C5V = speed;
+	}
+}
+
+void move_left_forward_wheel(uint16_t speed, uint8_t direction) {
+	if (direction) {
+		TPM1_C0V = speed;
+		TPM1_C1V = 0;
+	} else {
+		TPM1_C0V = 0;
+		TPM1_C1V = speed;
+	}
+}
+
+void move_left_backward_wheel(uint16_t speed, uint8_t direction) {
+	if (direction) {
+		TPM0_C2V = speed;
+		TPM0_C3V = 0;
+	} else {
+		TPM0_C2V = 0;
+		TPM0_C3V = speed;
+	}
+}
+
+void move_forward_or_backward(uint16_t speed, uint8_t direction) {
+  move_right_forward_wheel(speed, direction);
+	move_right_backward_wheel(speed, direction);
+  move_left_forward_wheel(speed, direction);
+	move_left_backward_wheel(speed, direction);
+}
+
+void turn_right_on_spot(uint16_t speed) {
+	move_right_forward_wheel(0, 1);
+	move_right_backward_wheel(speed, 0);
+	move_left_forward_wheel(speed, 1);
+	move_left_backward_wheel(speed, 1);
+}
+
+void turn_left_on_spot(uint16_t speed) {
+	move_right_forward_wheel(speed, 1);
+	move_right_backward_wheel(speed, 1);
+	move_left_forward_wheel(speed, 0);
+	move_left_backward_wheel(0, 1);
+}
+
+void turn_left_smooth(uint16_t base_speed, uint16_t turn_factor) {
+  move_left_forward_wheel(base_speed - turn_factor, 1);
+	move_left_backward_wheel(base_speed - turn_factor, 1);
+  move_right_forward_wheel(base_speed, 1);
+	move_right_backward_wheel(base_speed, 1);
+}
+
+void turn_right_smooth(uint16_t base_speed, uint16_t turn_factor) {
+	move_left_forward_wheel(base_speed, 1);
+	move_left_backward_wheel(base_speed, 1);
+	move_right_forward_wheel(base_speed - turn_factor, 1);
+	move_right_backward_wheel(base_speed - turn_factor, 1);
+}
+
+void stop_car() {
+  move_left_forward_wheel(0, 1);
+	move_left_backward_wheel(0, 1);
+  move_right_forward_wheel(0, 1);
+  move_right_backward_wheel(0, 1);
+}
+
+
+void move(int base_speed, int direction, int turn_factor){
+ /*
+	* Direction: 
+	* 0 - move forward
+	* 1 - move_backward
+	* 2 - on the spot left
+	* 3 - on the spot right
+	* 4 - smooth left
+	* 5 - smooth right
+	*/
+	
+	if (base_speed == 0) {
+		stop_car();
+	} else {
+		switch (direction) {
+			case 0:
+				move_forward_or_backward(base_speed, 1);
+				break;
+			case 1:
+				move_forward_or_backward(base_speed, 0);
+				break;
+			case 2:
+				turn_left_on_spot(base_speed);
+				break;
+			case 3:
+				turn_right_on_spot(base_speed);
+				break;
+			case 4:
+				turn_left_smooth(base_speed, turn_factor);
+				break;
+			case 5:
+				turn_right_smooth(base_speed, turn_factor);
+				break;
+			default:
+				break;
+		}
+	}
+	
+}
