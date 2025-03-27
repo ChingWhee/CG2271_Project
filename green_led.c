43c846d332a75128a5fbee2ac251e3ab336d3402
ChingWhee <wongchingwhee@gmail.com>
2025-03-27 12:04:05 +0800

    Fix green led

--- green_led.c
+++ green_led.c
@@ -1,23 +1,5 @@
 #include "green_led.h"
 
-// Green LEDs -- PTC0 - 7, 10 - 11
-#define GREEN_LED_0 0
-#define GREEN_LED_1 1
-#define GREEN_LED_2 2
-#define GREEN_LED_3 3
-#define GREEN_LED_4 4
-#define GREEN_LED_5 5
-#define GREEN_LED_6 6
-#define GREEN_LED_7 7
-#define GREEN_LED_8 10
-#define GREEN_LED_9 11
-
-#define RED_LED 8
-
-#define MASK(x) (1 << (x))
-
-int isMoving = 0;
-#define LED_DELAY 200
 extern volatile int moving_flag;
 	
 void InitLed(void)
